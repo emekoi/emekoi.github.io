a445e11e904014d24d8c48dcdb0a8873cc0c80df
Emeka Nkurumeh <emekankurumeh@outlook.com>
2023-12-15 16:05:18 -0800

    added fonts and tweaked css

--- site.hs
+++ site.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main
     ( main
@@ -20,8 +21,8 @@ import Network.HTTP.Types.Status  (status404)
 import Network.Wai                qualified as W
 import Slug
 import System.Directory           (doesFileExist)
-import System.FilePath            (takeExtension, (<.>), (</>))
-import System.Process
+import System.FilePath            (hasExtension, (<.>), (</>))
+import System.Process             (readProcess)
 import Text.RawString.QQ
 import Utils
 import WaiAppStatic.Types
@@ -29,29 +30,31 @@ import WaiAppStatic.Types
 -- https://github.com/LightAndLight/lightandlight.github.io/blob/a29bac1b084b86abe43e28c4062ca963d0647b98/site.hs#L31-L55
 hakyllConfig :: Configuration
 hakyllConfig = defaultConfiguration
-  { previewSettings =
-    \path -> let settings = previewSettings defaultConfiguration path in
-      settings
-        { ss404Handler = Just pageNotFound
+  { previewSettings = \path ->
+      let s@StaticSettings{..} = config.previewSettings path in s
+        { ss404Handler = Just \_ respond ->
+          B.readFile (config.destinationDirectory </> "404.html")
+            >>= respond . W.responseLBS status404 []
         , ssLookupFile = \pieces ->
           case splitAt (length pieces - 1) pieces of
-            (prefix, [piece])
-              | let fileName = fromPiece piece
-              , takeExtension (T.unpack fileName) == "" ->
-                  settings.ssLookupFile $ prefix <> [unsafeToPiece $ fileName <> ".html"]
-            _ -> settings.ssLookupFile pieces
+            (prefix, [fromPiece -> fileName])
+              | nullExtension (T.unpack fileName) ->
+                ssLookupFile $ prefix <> [unsafeToPiece $ fileName <> ".html"]
+            _ -> ssLookupFile pieces
         , ssGetMimeType = \file ->
-          if takeExtension (T.unpack (fromPiece file.fileName)) == "" then do
-            htmlExists <- doesFileExist $ path </> T.unpack (fromPiece file.fileName) <.> "html"
-            if htmlExists then pure "text/html" else settings.ssGetMimeType file
-          else settings.ssGetMimeType file
+          let fileName = T.unpack $ fromPiece file.fileName in
+          if nullExtension fileName then do
+            htmlExists <- doesFileExist $ path </> fileName <.> "html"
+            if htmlExists then pure "text/html" else ssGetMimeType file
+          else ssGetMimeType file
         }
   }
   where
-    pageNotFound ::  W.Application
-    pageNotFound _ respond =
-      B.readFile (destinationDirectory defaultConfiguration </> "404.html")
-        >>= respond . W.responseLBS status404 []
+    nullExtension :: FilePath -> Bool
+    nullExtension = not . hasExtension
+
+    config :: Configuration
+    config = defaultConfiguration
 
 feedConfig :: FeedConfiguration
 feedConfig = FeedConfiguration
@@ -87,6 +90,9 @@ tagPageEntry = [r|
   $partial("templates/post-list.html")$
 |]
 
+staticFiles :: Pattern
+staticFiles = foldr1 (.||.) ["fonts/**"]
+
 main :: IO ()
 main = do
   hakyllArgs <- defaultParser hakyllConfig
@@ -111,12 +117,42 @@ main = do
         Tag tag ->
           filterM (\(Item id _) -> elem tag <$> getTags id) posts
 
+    createFeed name feedT itemT f ctx =
+      create [name] $ version "generated" do
+        route idRoute
+        compile do
+          posts <- recentFirst =<< loadAllSnapshots allPostsPattern "content"
+          feedT <- loadBody . fromFilePath $ "templates" </> feedT
+          itemT <- loadBody . fromFilePath $ "templates" </> itemT
+          let feedCtx = bodyField "description" <> ctx <> postCtx
+          f feedT itemT feedConfig feedCtx posts
+
   hakyllWithArgs hakyllConfig hakyllArgs do
     match "templates/*" $
       compile templateBodyCompiler
 
     match ("css/*.css" .&&. complement "css/default.css") $
       compile templateBodyCompiler
+
+    -- NOTE: even though these are not in the final site (no `route`),
+    -- we need to use `copyFileCompiler` so they are in the store and
+    -- dependency tracking works correctly
+    match resumePattern do
+      compile copyFileCompiler
+
+    resumeDeps <- makePatternDependency resumePattern
+    rulesExtraDependencies [resumeDeps] do
+      create ["static/resume.pdf"] do
+        route idRoute
+
+        when isWatch do
+          compile $ do
+            _ <- unsafeCompiler $ readProcess "latexmk" [] []
+            makeItem ()
+
+    match staticFiles do
+      route idRoute
+      compile copyFileCompiler
 
     match "css/default.css" do
       route idRoute
@@ -141,22 +177,6 @@ main = do
         >>= loadAndApplyTemplate "templates/default.html" postCtx
         >>= relativizeUrls
 
-    -- NOTE: even though these are not in the final site (no `route`),
-    -- we need to use `copyFileCompiler` so they are in the store and
-    -- dependency tracking works correctly
-    match resumePattern do
-      compile copyFileCompiler
-
-    resumeDeps <- makePatternDependency resumePattern
-    rulesExtraDependencies [resumeDeps] do
-      create ["static/resume.pdf"] do
-        route idRoute
-
-        when isWatch do
-          compile $ do
-            _ <- unsafeCompiler $ readProcess "latexmk" [] []
-            makeItem ()
-
     draftsDep <- makePatternDependency draftsPattern
     rulesExtraDependencies [draftsDep] do
       match "pages/index.md" do
@@ -176,7 +196,7 @@ main = do
                 , defaultContext
                 ]
 
-          makeItem "$partial(\"templates/post-list.html\")$"
+          makeItem [r|$partial("templates/post-list.html")$|]
             >>= applyAsTemplate postListCtx
             >>= loadAndApplyTemplate "templates/default.html" postListCtx
             >>= relativizeUrls
@@ -193,6 +213,8 @@ main = do
               pageCtx = constField "title" "Tags" <> defaultContext
 
           uItem <- Item <$> getUnderlying
+
+          -- NOTE: foldrM to avoid quadratic slowdown
           page <- foldrM `flip` [] `flip` fold postTags $ \tag acc -> do
             Item _ item <- applyAsTemplate (postListCtx tag) $ uItem tagPageEntry
             pure $ item ++ acc
@@ -200,20 +222,8 @@ main = do
           loadAndApplyTemplate "templates/default.html" pageCtx (uItem page)
             >>= relativizeUrls
 
-      create ["feed.xml"] $ version "generated" do
-        route idRoute
-        compile do
-          feedT <- loadBody "templates/atom.xml"
-          itemT <- loadBody "templates/atom-item.xml"
-          posts <- recentFirst =<< loadAllSnapshots allPostsPattern "content"
-          let feedCtx = postCtx <> bodyField "description"
-          renderAtomWithTemplates feedT itemT feedConfig feedCtx posts
+      createFeed "feed.xml" "atom.xml" "atom-item.xml"
+        renderAtomWithTemplates mempty
 
-      create ["feed.json"] $ version "generated" do
-        route idRoute
-        compile do
-          posts <- recentFirst =<< loadAllSnapshots allPostsPattern "content"
-          feedT <- loadBody "templates/feed.json"
-          itemT <- loadBody "templates/feed-item.json"
-          let feedCtx = bodyField "description" <> jsonTagsCtx <> postCtx
-          renderJsonWithTemplates feedT itemT feedConfig feedCtx posts
+      createFeed "feed.json" "feed.json" "feed-item.json"
+        renderJsonWithTemplates jsonTagsCtx
